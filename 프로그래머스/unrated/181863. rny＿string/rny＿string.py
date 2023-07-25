88237fbcea75a5aa410051cf53bb5efaaa8b71d5
youkangjin <140174747+youkangjin@users.noreply.github.com>
2023-07-26 02:36:42 +0900

    [unrated] Title: rny_string, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181863. rny＿string/rny＿string.py
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.py
@@ -1,11 +1,3 @@
 def solution(rny_string):
-    i=0
-    a=rny_string.count("m")
-    if a==0:
-        return rny_string
-    else:
-        for i in range(i, a):
-            b=rny_string.replace("m", "rn")
-            i=i+1
-        return b
-            
+    answer = ''
+    return answer
